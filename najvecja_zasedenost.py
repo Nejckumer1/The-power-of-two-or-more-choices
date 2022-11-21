30c1c6b71726c002a64421b1e8c9ce349ec07248
Neža Lesnjak <nezalesnjak@gmail.com>
2022-11-21 10:26:45 +0100

    dodana funkcija za izvedbo poskusa

--- najvecja_zasedenost.py
+++ najvecja_zasedenost.py
@@ -30,9 +30,39 @@ def najvecje_stevilo_zog_v_kosu(N,n,d,stevilo_ponovitev):
         analiticno = log(log(n))/log(d) # kako izracunamo, ce je d >= 2
     return avg, analiticno
 
-
+"""
 print(najvecje_stevilo_zog_v_kosu(1000,1000,1,100))
 print(najvecje_stevilo_zog_v_kosu(1000,1000,2,100))
-print(najvecje_stevilo_zog_v_kosu(1000,1000,3,100))
-print(najvecje_stevilo_zog_v_kosu(1000,1000,4,100))
+"""
 
+def izvedi_funkcijo(Poskusi, N, n, d, stevilo_ponovitev):
+    # Poskusi je kolikokrat ponovimo funkcijo
+    ime_datoteke = "najvecja_zasedenost_" + str(N) + "_" + str(n) + "_" + str(d) + ".csv"
+    f = open(ime_datoteke, "w") # w --> Za na novo write, "a" --> za append
+    f.write("avg,analiticno  \n")
+    # Ce je Poskusi == 0 izvajamo do prekinitve, sicer omejeno
+    if Poskusi == 0:
+        while True:
+            avg, analiticno = najvecje_stevilo_zog_v_kosu(N, n, d, stevilo_ponovitev)
+            avg = round(avg, 4)
+            analiticno = round(analiticno, 4)
+            f.write(str(avg) + ","+ str(analiticno) + "\n")
+    else:
+        for poskus in range(Poskusi):
+            avg, analiticno = najvecje_stevilo_zog_v_kosu(N, n, d, stevilo_ponovitev)
+            avg = round(avg, 4)
+            analiticno = round(analiticno, 4)
+            f.write(str(avg) + "," + str(analiticno) + "\n")
+    f.close()
+
+
+izvedi_funkcijo(1000, 1000, 1000, 1, 1000)
+izvedi_funkcijo(1000, 10000, 10000, 1, 1000)
+izvedi_funkcijo(1000, 100000, 100000, 1, 1000)
+izvedi_funkcijo(1000, 1000000, 1000000, 1, 1000)
+
+izvedi_funkcijo(1000, 10000, 10000, 2, 1000)
+izvedi_funkcijo(1000, 10000, 10000, 3, 1000)
+izvedi_funkcijo(1000, 10000, 10000, 4, 1000)
+izvedi_funkcijo(1000, 10000, 10000, 5, 1000)
+izvedi_funkcijo(1000, 10000, 10000, 10, 1000)
