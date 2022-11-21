1f92517faaf7038f10f89f211d1bd35f51955c92
Janoš Vidali <janos.vidali@fmf.uni-lj.si>
2022-11-21 12:52:22 +0100

    Popravki programa

--- najvecja_zasedenost.py
+++ najvecja_zasedenost.py
@@ -1,12 +1,16 @@
-import random 
+import csv
+import random
+import time
 from math import log, log10 # lahko probamo menjat spodaj pri analiticnem izracunu
 
 def argmin(seznam):   # na katerem mestu je najmanj zog
-    n = len(seznam)
-    najmanjsi_element = min(seznam)
-    for i in range(n):
-        if seznam[i] == najmanjsi_element:
-            return i
+    _, i = min((x, i) for i, x in enumerate(seznam)) # bolj učinkovit način
+    return i
+    #n = len(seznam)
+    #najmanjsi_element = min(seznam)
+    #for i in range(n):
+    #    if seznam[i] == najmanjsi_element:
+    #        return i
 
 # N ... št. košev
 # n ... št. žog
@@ -14,21 +18,23 @@ def argmin(seznam):   # na katerem mestu je najmanj zog
 
 def najvecje_stevilo_zog_v_kosu(N,n,d,stevilo_ponovitev):
     max_zasedenosti = []
-    kosi = kosi = [i for i in range(N)]
+    kosi = range(N)
+    start = time.time() # zacetek merjenja casa
     for ponovitev in range(stevilo_ponovitev):
-        zoge = [0 for i in range(n)]        # seznam, ki predstavlja kose
+        zoge = [0] * N       # seznam, ki predstavlja kose
         for i in range(n):
             kateri = random.sample(kosi, d) # izberemo random d kosev
             koliko = [zoge[j] + (random.randint(0, 25) / 100) for j in kateri]  # pogledamo, koliko zog je v vsakem kosu  
             k = argmin(koliko)      # na katerem mestu je najmanj zog
             zoge[kateri[k]] += 1    # v pripadajoci kos vrzemo zogo
         max_zasedenosti.append(max(zoge))   # koliko zog je v najbolj zasedenem kosu
+    end = time.time() # konec merjenja casa
     avg = sum(max_zasedenosti)/len(max_zasedenosti) # povprecna najvecja zasedenost
     if d == 1:
         analiticno = log(n)/log(log(n)) # kako izracunamo, ce je d = 1
     else:
         analiticno = log(log(n))/log(d) # kako izracunamo, ce je d >= 2
-    return avg, analiticno
+    return avg, analiticno, end-start
 
 """
 print(najvecje_stevilo_zog_v_kosu(1000,1000,1,100))
@@ -37,23 +43,19 @@ print(najvecje_stevilo_zog_v_kosu(1000,1000,2,100))
 
 def izvedi_funkcijo(Poskusi, N, n, d, stevilo_ponovitev):
     # Poskusi je kolikokrat ponovimo funkcijo
-    ime_datoteke = "najvecja_zasedenost_" + str(N) + "_" + str(n) + "_" + str(d) + ".csv"
-    f = open(ime_datoteke, "w") # w --> Za na novo write, "a" --> za append
-    f.write("avg,analiticno  \n")
-    # Ce je Poskusi == 0 izvajamo do prekinitve, sicer omejeno
-    if Poskusi == 0:
-        while True:
-            avg, analiticno = najvecje_stevilo_zog_v_kosu(N, n, d, stevilo_ponovitev)
+    print("izvedi_funkcijo(%d, %d, %d, %d, %d)" % (Poskusi, N, n, d, stevilo_ponovitev))
+    ime_datoteke = "najvecja_zasedenost_%d_%d_%d.csv" % (N, n, d)
+    with open(ime_datoteke, "w") as f: # w --> Za na novo write, "a" --> za append
+        wr = csv.writer(f)
+        wr.writerow(["avg", "analiticno", "cas"])
+        # Ce je Poskusi == 0 izvajamo do prekinitve, sicer omejeno
+        i = 1
+        while i != Poskusi - 1:
+            avg, analiticno, cas = najvecje_stevilo_zog_v_kosu(N, n, d, stevilo_ponovitev)
             avg = round(avg, 4)
             analiticno = round(analiticno, 4)
-            f.write(str(avg) + ","+ str(analiticno) + "\n")
-    else:
-        for poskus in range(Poskusi):
-            avg, analiticno = najvecje_stevilo_zog_v_kosu(N, n, d, stevilo_ponovitev)
-            avg = round(avg, 4)
-            analiticno = round(analiticno, 4)
-            f.write(str(avg) + "," + str(analiticno) + "\n")
-    f.close()
+            wr.writerow([avg, analiticno, cas])
+            f.flush() # vrstica naj se zapiše takoj
 
 
 izvedi_funkcijo(1000, 1000, 1000, 1, 1000)
